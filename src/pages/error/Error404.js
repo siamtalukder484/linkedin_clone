526ffb37688a23fa65c7ee3bf152eb92bd0efc72
siamtalukder484 <siamtalukder484@gmail.com>
2023-06-03 16:29:55 +0600

    error page go back operation

--- src/pages/error/Error404.js
+++ src/pages/error/Error404.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './error.css'
+import { useNavigate } from 'react-router-dom';
 
 const Error404 = () => {
+
+    const navigate = useNavigate();
+
   return (
     <>
         <div className='error_main'>
+            <button onClick={() => navigate(-1)}>Back to Previous Page</button>
             <h1>404 Error</h1>
             <h1>Page Not Found</h1>
         </div>
